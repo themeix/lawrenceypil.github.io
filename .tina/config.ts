8a491decae093d527e3311836c9a42a92068a891
Scott Lee Chua <scottleechua@gmail.com>
2023-03-06 17:33:57 +0800

    Add Tina

---
+++ .tina/config.ts
@@ -0,0 +1,118 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.CLIENT_ID,
+  token: process.env.READ_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "_posts",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "categories",
+            label: "Categories",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          filename: {
+            readonly: true,
+            slugify: values => {
+              const date = new Date();
+              const day = ('0' + date.getDate()).slice(-2);
+              const month = ('0' + (date.getMonth()+1)).slice(-2);
+              const year = date.getFullYear();
+        
+              let currentDate = `${year}-${month}-${day}`;
+        
+              return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
+            }
+          }
+        },
+      },
+      {
+        name: "page",
+        label: "Pages",
+        path: "_pages",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "permalink",
+            label: "Permalink",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          filename: {
+            readonly: true,
+            slugify: values => {        
+              return `${values?.title?.toLowerCase().replace(/ /g, '-')}`
+            }
+          }
+        },
+      },
+    ],
+  },
+});
